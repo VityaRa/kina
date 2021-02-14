d6144c298905b6d34a590ad3293ef9dbb2ec4faa
VityaRa <blackops2in@mail.ru>
2021-02-15 00:33:59 +0300

    added info

--- show-filmes-jenre.js
+++ show-filmes-jenre.js
@@ -1,10 +1,65 @@
 let filmList = [
   {
-    name: "SomeName",
-    rating: 4.5,
+    name: "Зелёная миля",
+    rating: 9.1,
+    jenre: ['Драма', 'Фантастика'],
+    imgName: '1.webp'
+  }, 
+  {
+    name: "Побег из Шоушенка",
+    rating: 9.5,
     jenre: 'Драма',
-    imgName: '1.jpg'
-  }
+    imgName: '10.webp'
+  },
+  {
+    name: "Властелин колец",
+    rating: 8.1,
+    jenre: ['Фантастика', 'Приключения'],
+    imgName: '2.webp'
+  },
+  {
+    name: "Интерстеллар",
+    rating: 8.7,
+    jenre: ['Фантастика', 'Драма'],
+    imgName: '3.webp'
+  },
+  {
+    name: "Форрест Гамп",
+    rating: 9.5,
+    jenre: ['Драма', 'Мелодрама'],
+    imgName: '4.webp'
+  },
+  {
+    name: "Список Шиндлера",
+    rating: 8.8,
+    jenre: ['Драма', 'Трагедия'],
+    imgName: '5.webp'
+  },
+  {
+    name: "1+1",
+    rating: 9.7,
+    jenre: ['Драма', 'Комедия'],
+    imgName: '6.webp'
+  },
+  {
+    name: "Назад в будущее",
+    rating: 9.8,
+    jenre: ['Комедия', 'Фантастика'],
+    imgName: '7.webp'
+  },
+  {
+    name: "Король Лев",
+    rating: 7.8,
+    jenre: ['Приключение', 'Трагедия'],
+    imgName: '8.webp'
+  },
+  {
+    name: "Криминальное чтиво",
+    rating: 8.9,
+    jenre: ['Триллер', 'Комедия'],
+    imgName: '9.webp'
+  },
+
 ]
 
 let currentFilmList = []
@@ -13,7 +68,7 @@ let selectedClass = 'selected'
 
 function makeFilmCard (name, rating, imgName) {
   return `<div class="film-card">
-      <div style = "background-image: ./film-images/${imgName}" class="film-image">
+      <div style = "background-image: url(./film-images/${imgName});" class="film-image">
 
       </div>
       <div class="film-about">
@@ -23,21 +78,24 @@ function makeFilmCard (name, rating, imgName) {
     </div>`
 }
 
+
 function deleteElemByClass (className) {
   //add animation!!!
-  let elem = document.getElementsByClassName(className)[0]
-  elem.parentNode.removeChild(elem)
+  if (document.getElementsByClassName(className).length != 0) {
+    let elem = document.getElementById("filmList");
+    while (elem.firstChild) {
+      elem.removeChild(elem.firstChild);
+    }
+  }
 }
-
-
-
 
 function clearSelection (obj){
   for (elem of obj) elem.classList.remove(selectedClass)
 }
 
+
 function showFilms(jenreName){
-  currentFilmList = filmList.filter(film => film.jenre == jenreName)
+  currentFilmList = filmList.filter(film => film.jenre.includes(jenreName))
   let filmListElem = document.getElementsByClassName('film-list')[0]
 
   for (filmElem of currentFilmList) {
@@ -46,12 +104,19 @@ function showFilms(jenreName){
   currentFilmList = []
 }
 
+function delay(callback) {
+  setTimeout(callback, 1000)
+}
+
+
+
 function showJenre(e){
   if (!e.classList.contains(selectedClass)) {
     clearSelection(document.getElementsByClassName('jenre-elem'))
     e.classList.add(selectedClass)
+
+    deleteElemByClass('film-card')
     document.getElementsByClassName('removing-title')[0].innerHTML = e.innerHTML
-    //deleteElemByClass('removing-title')
     showFilms(e.innerHTML)
   }
 }
